d500b70ae8789be905bb23acc693644de07a3d8f
James Andrews <jbandrew14@gmail.com>
2021-10-12 16:15:41 -0400

    connected other .js files through html; all content is added to page through main.js using for loop and template literals; used moment.js to convert dates and a function to capitalize names

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
-function createCustCard(customer) {
-    let allCust = document.getElementById("all-customers");
-    let custCard = document.createElement("div")
-    custCard.appendChild(createImgElement(customer));
-    custCard.appendChild(createNameElement(customer));
-    custCard.appendChild(createEmailElement(customer));
-    custCard.appendChild(createFirstAddressEl(customer));
+let container = document.getElementById("all-customers");
+
+for (customer of customers) {
+    let custCard = document.createElement("div");
+
+    custCard.innerHTML = `<img src=${customer.picture.large} />
+    <p>${capitalizeName(customer.name.first)} ${capitalizeName(customer.name.last)}</p>
+    <p>${customer.email}</p>
+    <p>${customer.location.street.number} ${customer.location.street.name}</p>
+    <p>${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}</p>
+    <p>DOB: ${moment(customer.dob.date).format("MMM D, YYYY")}</p>
+    <p>Customer Since: ${moment(customer.registered.date).format("MMM D, YYYY")}</p>`
+    
+    container.appendChild(custCard);
 }
 
-function createImgElement(customer) {
-    let image = document.createElement("img");
-    image.src = customer.picture.thumbnail;
-    return image;
+function capitalizeName(name) {
+    let first = name[0];
+    first = first.toUpperCase();
+    return first + name.slice(1, name.length);
 }
-
-function createNameElement(customer) {
-    let name = document.createElement("p");
-    name.innerText = customer.name.first + " " + customer.name.last;
-    return name
-}
-
-function createEmailElement(customer) {
-    let email = document.createElement("p");
-    email.innerText = customer.email;
-    return email;
-}
-
-function createFirstAddressEl(customer) {
-    let firstLine = document.createElement("p");
-    firstLine.innerText = String(customer.location.street.number) + " " + customer.location.street.name;
-    return firstLine;
-}
+// string.replace(/^\w/, c => c.toUpperCase()) also works, but I don't understand it. Something to do with a RegEx
